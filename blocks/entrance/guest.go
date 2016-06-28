d989482241e812d076a755ef6e9f4a25fc25f917
wonseobjoe <wsjoe78@gmail.com>
2016-06-29 01:08:20 +0900

    added guest.go into blocks/entrance/

---
+++ blocks/entrance/guest.go
@@ -0,0 +1,23 @@
+/*
+	blocks/main/guest.go
+*/
+
+package main
+
+import (
+	"../archi/bottom"
+	"fmt"
+)
+
+func PressBellByGuest(arg string) {
+	//bottom.Greeting()
+	bottom.GreetingWithHello(arg)
+}
+
+func main() {
+	var your_name string
+	fmt.Print("Say your name: ")
+	fmt.Scanln(&your_name)
+
+	PressBellByGuest(your_name)
+}
