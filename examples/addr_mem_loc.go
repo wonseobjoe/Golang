51f69d6ebf2f80be509c37b23b5565e76eefc77e
wonseobjoe <wsjoe78@gmail.com>
2016-06-29 16:07:01 +0900

    added addr_mem_loc.go to examples

---
+++ examples/addr_mem_loc.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	i := 5
+	fmt.Println("i is: ", i)
+	fmt.Println("address of i is: ", &i)
+
+	ptr := &i
+	fmt.Println("address of pointer to i: ", ptr)
+	fmt.Println("value of pointer to i: ", *ptr)
+}
